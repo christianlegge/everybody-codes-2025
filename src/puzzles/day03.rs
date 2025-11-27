3957b316d53ae62a6fd075ea0bd91e4b8291ac1d
Christian Legge <christian@christianlegge.dev>
2025-11-27 15:55:01 -0500

    day 3 pt 1-2

--- src/puzzles/day03.rs
+++ src/puzzles/day03.rs
@@ -1,3 +1,4 @@
+use hashbrown::HashSet;
 use regex::Regex;
 
 #[derive(Debug)]
@@ -31,25 +32,16 @@ impl MyNumber {
 
 pub fn solve(data: String) {
     println!("Text input: {}", data);
-    let re = Regex::new(r"A=\[(\d+),(\d+)\]").unwrap();
-    let Some(caps) = re.captures(&data) else {
-        panic!("invalid format for number")
-    };
-
-    let start = MyNumber {
-        x: (&caps[1]).parse::<i64>().unwrap(),
-        y: (&caps[2]).parse::<i64>().unwrap(),
-    };
-
-    let mut res = MyNumber { x: 0, y: 0 };
-
-    for _ in 0..3 {
-        res = res.mult(&res);
-        res = res.div(&MyNumber { x: 10, y: 10 });
-        res = res.add(&start);
+    let crates = data
+        .split(",")
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+    let mut crate_set = HashSet::new();
+    for ele in crates {
+        crate_set.insert(ele);
     }
-
-    println!("final result: {:#?}", res);
+    let sum = crate_set.into_iter().reduce(|a, v| a + v);
+    println!("sum: {:#?}", sum);
 }
 
 fn result_exceeds(num: &MyNumber) -> bool {
@@ -77,31 +69,23 @@ fn should_engrave(num: &MyNumber) -> bool {
 
 pub fn solve2(data: String) {
     println!("Text input: {}", data);
-    let re = Regex::new(r"A=\[(-?\d+),(-?\d+)\]").unwrap();
-    let Some(caps) = re.captures(&data) else {
-        panic!("invalid format for number")
-    };
-
-    let start = MyNumber {
-        x: (&caps[1]).parse::<i64>().unwrap(),
-        y: (&caps[2]).parse::<i64>().unwrap(),
-    };
-
-    let mut engraving = 0;
-
-    for i in 0..101 {
-        for j in 0..101 {
-            let point = start.add(&MyNumber {
-                x: 10 * i,
-                y: 10 * j,
-            });
-            if should_engrave(&point) {
-                engraving += 1;
-            }
+    let mut crates = data
+        .split(",")
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+    let mut crate_set = HashSet::new();
+    crates.sort();
+    for ele in crates {
+        if crate_set.contains(&ele) {
+            continue;
+        }
+        crate_set.insert(ele);
+        if crate_set.len() == 20 {
+            break;
         }
     }
-
-    println!("final result: {:#?}", engraving);
+    let sum = crate_set.into_iter().reduce(|a, v| a + v);
+    println!("sum: {:#?}", sum);
 }
 
 pub fn solve3(data: String) {
